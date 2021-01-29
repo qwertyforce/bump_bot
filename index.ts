86beb87c92dee4c47c1a7d0c5916c971f1e26cc2
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2021-01-29 22:50:00 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,49 +1,55 @@
 import { Solver } from "2captcha"
 import axios from "axios"
 import FormData from 'form-data'
-import tunnel from 'tunnel'
-// import SocksProxy from "socks-proxy-agent"
 const solver = new Solver("")
-// const httpsAgent1 = SocksProxy(`socks5://127.0.0.1:8888/`);
-// const client1 = axios.create({httpsAgent:httpsAgent1});
-// const asfqwf:any=(tunnel as any).httpsOverHttp({ 
-//     proxy:{
-//     host: '127.0.0.1',
-//     port: 8888,
-// },
-// rejectUnauthorized: false
-// })
+const board="mlp"
+const post_url="https://sys.4channel.org/tv/post"
+const board_url="https://boards.4channel.org"
 
-//  const axiosInstance = axios.create({
-//     httpsAgent: asfqwf,
-    
-//     proxy: false,
-//   });
- 
-async function send_post(msg:string){
-    const captcha_res=await solver.recaptcha("6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc", "https://boards.4channel.org/mlp/thread/36306000")
-    console.log(captcha_res)
-    const form = new FormData();
-    form.append("MAX_FILE_SIZE", '4194304');
-    form.append("mode", 'regist');
-    form.append("resto", '36306000');//thread number
-    form.append("name", '');//name
-    form.append("email", '');//options
-    form.append("com", msg);//text message
-    form.append("g-recaptcha-response",captcha_res.data);
-
-    try{
-        const response = await axios.post("https://sys.4channel.org/mlp/post",form.getBuffer(),{
-            headers:{ ...form.getHeaders(),'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' }
+async function send_post(msg:string,thread_num:string){
+    try {
+        const captcha_res=await solver.recaptcha("6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc", `${board_url}/${board}/thread/${thread_num}`)
+        console.log(captcha_res)
+        const form = new FormData();
+        form.append("MAX_FILE_SIZE", '4194304');
+        form.append("mode", 'regist');
+        form.append("resto", thread_num);//thread number
+        form.append("name", '');//name
+        form.append("email", '');//options
+        form.append("com", msg);//text message
+        form.append("g-recaptcha-response",captcha_res.data)
+        // const x = await axiosInstance.options("https://sys.4channel.org/qa/post")
+        // console.log(x.data)
+        const response = await axios.post(post_url, form.getBuffer(), {
+            headers: { ...form.getHeaders(),
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language":": en-US,en;q=0.5",
+                'Origin': board_url,
+                'Referer':`${board_url}/`,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' }
         })
         console.log(response.data)
-    }catch(e){
+    } catch (e) {
         console.log(e)
         console.log(e?.response?.data)
     }
 }
 
-async function run(){
- await send_post("bump")
-}
-run()
+const threads:number[]=[]
+const interval = setInterval(async function () {
+        const resp=await axios.get(`https://a.4cdn.org/${board}/threads.json`)
+        const resp_data = resp.data
+        for (const page of resp_data){
+            for(const thread of page){
+                if(threads.includes(thread.no)){
+                    if(page==10){
+                        send_post("bump",thread.no)
+                    }
+                }
+            }
+        }
+        // for(const thread of threads){
+           
+        // }
+    }, 60000);
